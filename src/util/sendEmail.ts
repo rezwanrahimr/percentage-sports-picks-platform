7a96b9c9b1a5a6b22c92e6fc721a22f8fdf24613
Rezwan Rahim Rupak <rezwanrahim.rupak@gmail.com>
2025-08-13 16:15:38 +0600

    latest update

--- src/util/sendEmail.ts
+++ src/util/sendEmail.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer';
-import config from '../config';
+import {config} from '../config';
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
