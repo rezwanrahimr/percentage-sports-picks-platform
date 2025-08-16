7b58c36b58764f7479aa3880fd3b6a989487947b
Rezwan Rahim Rupak <rezwanrahim.rupak@gmail.com>
2025-08-16 12:46:03 +0600

    latest update for deploy

--- src/modules/userSubscription/userSubscription.service.ts
+++ src/modules/userSubscription/userSubscription.service.ts
@@ -4,8 +4,8 @@ import { UserSubscriptionModel } from "./userSubscription.model";
 const getUserById = async (userId: string) => {
     console.log("Fetching subscription for user:", userId);
     const id = idConverter(userId);
-    const result = await UserSubscriptionModel.findOne({ userId: id });
-    if (!result) {
+    const result = await UserSubscriptionModel.find({ userId: id });
+    if (!result || result.length === 0) {
         throw new Error("Subscription not found");
     }
     return result;
